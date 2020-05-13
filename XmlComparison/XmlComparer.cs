5ca283df82d59d7bc05613feee8b0bb20e3b44a2
Larry Gasik <larry@lgproductions.us>
2020-05-12 23:04:20 -0500

    Added References and sample files

---
+++ XmlComparison/XmlComparer.cs
@@ -0,0 +1,14 @@
+﻿using Org.XmlUnit.Builder;
+using Org.XmlUnit.Diff;
+
+namespace XmlComparison
+{
+    public static class XmlComparer
+    {
+        public static bool AreXMLDocumentsTheSame(string control, string test)
+        {
+            Diff myDiff = DiffBuilder.Compare(control).WithTest(test).Build();
+            return !myDiff.HasDifferences();
+        }
+    }
+}
---
+++ XmlComparisonTests/XMLComparerTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+
+namespace XmlComparisonTests
+{
+    [TestFixture()]
+    public class XMLComparerTests
+    {
+        
+
+        [Test]
+        public void ShouldPass()
+        {
+            string control = "<a><b attr1=\"abc\" attr2=\"def\"></b></a>";
+            string test = "<a><b attr1=\"uvw\" attr2=\"xyz\"></b></a>";
+
+            var result = XmlComparison.XmlComparer.AreXMLDocumentsTheSame(control, test);
+            Assert.AreEqual(false, result);
+        }
+    }
+}
